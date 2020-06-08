d72e009c29f199bc6bc3395b966eac845de488d1
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-06-08 12:39:21 +0300

    Add possibility to add new employee

--- src/main/java/com/marchenko/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/marchenko/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.marchenko.springboot.thymeleafdemo.dao;
 import com.marchenko.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAllByOrderByLastNameAsc();
 }
